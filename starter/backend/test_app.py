06b39ed3b6bae92177a8a4f4390954a59d6d126e
namfanmu <nguyenbanam.281999@gmail.com>
2024-05-14 20:19:37 +0700

    update backend ci

--- starter/backend/test_app.py
+++ starter/backend/test_app.py
@@ -5,9 +5,8 @@ import os
 def test_movies_endpoint_returns_200():
     try:
         with app.test_client() as client:
-            status_code = os.getenv("FAIL_TEST", 200)
+            status_code = int(os.getenv("FAIL_TEST", 200))
             response = client.get("/movies/")
             assert response.status_code == status_code
     except Exception:
         return True
-    
