867c0b8edc51897c96f42be51e6d65726c634305
namfanmu <nguyenbanam.281999@gmail.com>
2024-05-14 16:37:17 +0700

    update backend ci

--- starter/backend/__init__.py
+++ starter/backend/__init__.py
@@ -2,11 +2,8 @@ import os
 from flask import Flask
 from flask_cors import CORS
 
-from backend.movies import movies_api
-
 app = Flask(__name__)
 CORS(app)
-app.register_blueprint(movies_api)
 
 # Start app
 if __name__ == "__main__":
